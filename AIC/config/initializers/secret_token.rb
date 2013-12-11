04c1a56ca95d1f2a9dc6087fa66dea6c85c49577
Kurt Robert Rudolph <krudolph9@gmail.com>
2013-12-10 19:37:06 -0600

    added a rails app

---
+++ AIC/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AIC::Application.config.secret_key_base = '6fa630fc268153bab8a40e1cd477f337f83bf2d696b77e51a18f6357ee195a9217f61445b94c136961e2d9938be93683f38c68bf129f0bff42ef86e56dc5f5f0'
